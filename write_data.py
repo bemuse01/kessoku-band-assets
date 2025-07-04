e7b8273c79caddcf08960d381cfb1e3054e03127
bemuse01 <sequeland@gmail.com>
2025-07-05 07:50:48 +0900

    update

--- write_data.py
+++ write_data.py
@@ -1,9 +1,9 @@
+from PIL import Image
 import json
 import os
 
 data_name = 'data.json'
 main_path = './assets/images/'
-files = os.listdir(main_path)
 
 def load_json():
     with open(data_name, 'r') as file:
@@ -14,20 +14,43 @@ def save_json(data={}):
     with open(data_name, 'w') as file:
         json.dump(data, file, indent=4)
 
+def create_data(files):
+    temp = []
+    for file in files:
+        file_path = os.path.join(main_path, file)
+        image = Image.open(file_path)
+        width, height = image.size
+
+        temp.append(
+            {
+                'name': file,
+                'width': width,
+                'height': height
+            }
+        )
+    return temp
+
+
 def init():
-    data = load_json()
+    files = os.listdir(main_path)
 
-    body_data = [
-        {
-            'name': file,
-            'tag': [],
-            'type': 'image'
+    json_data = {
+        'head': {
+            'schema': {
+                'name': 'string',
+                'width': 'number',
+                'height': 'number'
+            }
+        },
+        'body': {
+            'data': []
         }
-        for file in files
-    ]
+    }
 
-    data['body']['data'] = body_data
+    body_data = create_data(files);
 
-    save_json(data)
+    json_data['body']['data'] = body_data
+
+    save_json(json_data)
 
 init()
